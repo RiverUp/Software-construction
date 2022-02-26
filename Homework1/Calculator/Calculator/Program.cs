74de5f0e6d46d9e0f7788f0e1ecebc2ece0d56d1
Candy <1940918445@qq.com>
2022-02-26 19:57:46 +0800

    software construction homework 1&2

---
+++ Homework1/Calculator/Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input an expression in such a way:1 + 2");
+            string all = Console.ReadLine();
+            string[] elements = all.Split(" ");
+            int[] eles = new int[2];
+            int res = 0;
+            string op;
+            eles[0] = Int32.Parse(elements[0]);
+            eles[1] = Int32.Parse(elements[2]);
+            op = elements[1];
+            switch (op)
+            {
+                case "+": res = eles[0] + eles[1]; break;
+                case "-": res = eles[0] - eles[1]; break;
+                case "*": res = eles[0] * eles[1]; break;
+                case "/": res = eles[0] / eles[1]; break;
+            }
+            Console.WriteLine($"the result is {res}");
+
+        }
+    }
+} 
+
---
+++ Homework2/PrimeFactor/PrimeFactor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PrimeFactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number");
+            string s = Console.ReadLine();
+            int Multiple = Int32.Parse(s);
+            if (Multiple < 2)
+                Console.WriteLine("it doesn't have a prime factor.");
+            for(int i=2;Multiple>=i ;i++)
+            {
+                int j = 0;
+                while(Multiple%i==0)
+                {
+                    Multiple /= i;
+                    if (j == 0)
+                    {
+                        Console.WriteLine($"{i} is its prime factor");
+                        j++;
+                    }
+                }
+            }
+        }
+    }
+}
