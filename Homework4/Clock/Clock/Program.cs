cd05041595f0a4fb59330e460ba1cba9b12fb36f
Candy <1940918445@qq.com>
2022-03-11 11:55:02 +0800

    homework4

---
+++ Homework4/Clock/Clock/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Clock
+{
+    public delegate void TickHandler(object sender);
+    public delegate void ClockHandler(object sender);
+    public class Clock
+    {
+        public Date d;
+        public Clock()
+        {
+            d = new Date();
+        }
+        public DateTime Alarm { set { d.Dalarm = value; d.onClock += new ClockHandler(d_onClock); } }
+        public void Run()
+        {
+            try
+            {
+                if (d.Dt > d.Dalarm)
+                {
+                    throw new Exception();
+                }
+                d.onTick += new TickHandler(d_Tick);
+                d.onClock += new ClockHandler(d_onClock);
+                while (d.Dt.Hour != d.Dalarm.Hour || d.Dt.Minute != d.Dalarm.Minute || d.Dt.Second != d.Dalarm.Second)
+                {
+                    d.Tick();
+                }
+                d.Clock();
+            }
+            catch
+            {
+                Console.WriteLine("the clock time input is wrong");
+            }
+        }
+        void d_Tick(object sender)
+        {
+            Console.WriteLine(d.Dt);
+        }
+        void d_onClock(object sender)
+        {
+            Console.WriteLine("it's time!!!!!");
+        }
+        
+    }
+    public class Date
+    {
+        DateTime dt;
+        DateTime dalarm;
+        public DateTime Dalarm { get { return dalarm;} set { dalarm = value; } }
+        public DateTime Dt { get { return dt; } }
+        public Date()
+        {
+            dt = DateTime.Now;
+        }
+
+        public event TickHandler onTick;
+        public event ClockHandler onClock;
+        public void Tick()
+        {
+            DateTime temp = DateTime.Now;
+            if (temp.Hour> dt.Hour||temp.Minute>dt.Minute||temp.Second>dt.Second)
+            {
+                dt = temp;
+                onTick(this);
+            }
+        }
+        public void Clock()
+        {
+            if (DateTime.Now != dalarm)
+            {
+                onClock(this);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock c = new Clock();
+            Console.WriteLine("please input your clock time:(xx.xx.xx)");
+            String s=Console.ReadLine();
+            String[] s2 = new String[3];
+            s2 = s.Split(".");
+            double hour, min, sec;
+            DateTime date = DateTime.Now;
+            hour = Double.Parse(s2[0]) - date.Hour;
+            min = Double.Parse(s2[1]) - date.Minute;
+            sec = Double.Parse(s2[2]) - date.Second;
+            c.d.Dalarm = date.AddHours(hour).AddMinutes(min).AddSeconds(sec);
+            c.Run();
+        }
+    }
+}
---
+++ Homework4/List/List/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace List
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head { get => head; }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public static void ForEach<T>(GenericList<T> g,Action<T> action)
+        {
+            Node<T> node;
+            node = g.Head;
+            while (node.Next != null)
+            {
+                action(node.Data);
+                node = node.Next;
+            }
+            action(node.Data);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<double> gener = new GenericList<double>();
+            gener.Add(1.4);
+            gener.Add(11.6);
+            gener.Add(3.0);
+            double max = gener.Head.Data,min = gener.Head.Data, sum = 0;
+            Action<double> action = (m => Console.WriteLine(m));
+            action += (m => sum += m);
+            action += m =>
+            {
+                if (gener.Head.Next.Data < min)
+                {
+                    min = gener.Head.Next.Data;
+                }
+            };
+            action += m =>
+            {
+                if (gener.Head.Next.Data > max)
+                {
+                    max = gener.Head.Next.Data;
+                }
+            };
+            GenericList<double>.ForEach(gener, action);
+            Console.WriteLine($"the sum of this list is {sum}");
+            Console.WriteLine($"the max of this list is {max}");
+            Console.WriteLine($"the min of this list is {min}");
+        }
+    }
+}
