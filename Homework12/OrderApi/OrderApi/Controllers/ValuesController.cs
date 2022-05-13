b9ba6b18a6dbbf4861dbb08443ea3f6774203ca5
Candy <1940918445@qq.com>
2022-05-13 22:28:55 +0800

    12th

---
+++ Homework12/OrderApi/OrderApi/Controllers/ValuesController.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OrderApi.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrderApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private  readonly OrderContext _orderContext;
+        public ValuesController(OrderContext context)
+        {
+            _orderContext = context;
+        }
+        // GET: api/todo/{id}  id为路径参数
+        [HttpGet("{id}")]
+        //根据id获取订单
+        public ActionResult<Order> GetOrder(long id)
+        {
+            var order = _orderContext.Orders.FirstOrDefault(t => t.OrderId == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return order;
+        }
+        [HttpPost]
+        //增加订单
+        public ActionResult<Order> AddOrder(Order newOrder)
+        {
+            try
+            {
+                _orderContext.Orders.Add(newOrder);
+                _orderContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException.Message);
+            }
+
+            return newOrder;
+        }
+
+        [HttpGet]
+        //Get全部的Order
+        public ActionResult<List<Order>> GetOrders()
+        {
+            return _orderContext.Orders .ToList();
+        }
+
+        [HttpDelete("{id}")]
+        //删除某id的订单
+        public ActionResult<Order> DeleteOrder(long id)
+        {
+            try
+            {
+                var order = _orderContext.Orders .FirstOrDefault(o => o.OrderId == id);
+                if (order != null)
+                {
+                    foreach (OrderDetail detail in order.Details)
+                    {
+                        detail.item = null;
+                    }
+                    _orderContext.Orders.Remove(order);
+                    _orderContext.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException.Message);
+            }
+            return NoContent();
+        }
+        [HttpGet("pageQuery")]
+        //查询某个id的order
+        public ActionResult<List<Order>> queryTodoItem(int orderid)
+        {
+            var query = buildQuery(orderid);
+            return query.ToList();
+        }
+        private IQueryable<Order> buildQuery(int orderid)
+        {
+            IQueryable<Order> query = _orderContext.Orders;
+            if (orderid != null)
+            {
+                query = query.Where(o => o.OrderId == orderid);
+            }
+            return query;
+        }
+        [HttpPut("{id}")]
+        public ActionResult<Order> changeOrder(long id, Order order)
+        {
+            if (id != order.OrderId)
+            {
+                return BadRequest("Id cannot be modified!");
+            }
+            try
+            {
+                _orderContext.Entry(order).State = (System.Data.Entity.EntityState)EntityState.Modified;
+                _orderContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+                if (e.InnerException != null) error = e.InnerException.Message;
+                return BadRequest(error);
+            }
+            return NoContent();
+        }
+    }
+}
---
+++ Homework12/OrderApi/OrderApi/models/Classes.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApi.models
+{
+    public class Goods
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public Goods(string name,int price)
+        {
+            Name = name; Price = price; 
+        }
+        public Goods() { }
+
+    }
+    public class OrderDetail
+    {
+        public int OrderDetailId { get; set; }
+        public int OrderId { get; set; }
+        [Required]
+        public int itemNums { get; set; }
+        public Goods item { get; set; }
+        public Order Order { get; set; }
+        public OrderDetail(Goods go, int itemNum,int id)
+        {
+
+            OrderDetailId = id;
+            this.item = go;
+            this.itemNums = itemNum;
+        }
+        public OrderDetail() { }
+    }
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int Money { get; set; }
+        public List<OrderDetail> Details { get; set; }
+        public Order(int orderId)
+        {      
+            OrderId = orderId;
+        }
+        public Order() { }
+    }
+}
+
